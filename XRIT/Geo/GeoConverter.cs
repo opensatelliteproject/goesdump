dafa7ae3508097c16da43d05c160152094b02e4a
Lucas Teske <lucas@teske.net.br>
2017-04-29 23:55:02 -0300

    Added ShapeFile Map Drawer and GeoTools for converting coordinates

---
+++ XRIT/Geo/GeoConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OpenSatelliteProject.Geo {
+    public class GeoConverter {
+        private int coff;
+        private int loff;
+        private float cfac;
+        private float lfac;
+        private float satelliteLongitude;
+
+        /// <summary>
+        /// Maximum Visible Latitude
+        /// </summary>
+        /// <value>The max latitude.</value>
+        public float MaxLatitude { 
+            get {
+                return 79;
+            }
+        }
+
+        /// <summary>
+        /// Minimum Visible Latitude
+        /// </summary>
+        /// <value>The minimum latitude.</value>
+        public float MinLatitude { 
+            get {
+                return -79;
+            }
+        }
+
+        /// <summary>
+        /// Maximum visible Longitude
+        /// </summary>
+        /// <value>The max longitude.</value>
+        public float MaxLongitude {
+            get {
+                return satelliteLongitude + 79;
+            }
+        }
+
+        /// <summary>
+        /// Minimum visible latitude
+        /// </summary>
+        /// <value>The minimum longitude.</value>
+        public float MinLongitude {
+            get {
+                return satelliteLongitude - 79;
+            }
+        }
+
+        public GeoConverter(float satelliteLongitude, int coff, int loff, float cfac, float lfac) {
+            this.satelliteLongitude = satelliteLongitude;
+            this.coff = coff;
+            this.loff = loff;
+            this.cfac = cfac;
+            this.lfac = lfac;
+        }
+
+        public Tuple<int, int> latlon2xy(float lat, float lon) {
+            return GeoTools.lonlat2xy(satelliteLongitude, GeoTools.deg2rad(lon), GeoTools.deg2rad(lat), coff, cfac, loff, lfac);
+        }
+
+        public Tuple<float, float> xy2latlon(int x, int y) {
+            var radCoord = GeoTools.xy2lonlat(satelliteLongitude, x, y, coff, cfac, loff, lfac);
+            return new Tuple<float, float>(GeoTools.rad2deg(radCoord.Item1), GeoTools.rad2deg(radCoord.Item2));
+        }
+    }
+}
+
---
+++ XRIT/Geo/MapDrawer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DotSpatial.Data;
+using System.Drawing;
+
+namespace OpenSatelliteProject.Geo {
+    public class MapDrawer {
+        private Shapefile shapeFile;
+        
+        public MapDrawer(string shapeFile) {
+            this.shapeFile = Shapefile.OpenFile(shapeFile);
+        }
+
+        ~MapDrawer() {
+            shapeFile.Close();
+        }
+
+        public void DrawMap(ref Bitmap bmp, GeoConverter gc, Color color, int lineWidth = 5) {
+            Pen pen = new Pen(color, lineWidth);
+            float lastX = -1;
+            float lastY = -1;
+            using (var graphics = Graphics.FromImage(bmp)) {
+                foreach (var f in shapeFile.Features) {
+                    for (int i = 0; i < f.NumGeometries; i++) {
+                        var geom = f.GetBasicGeometryN(i);
+                        var k = geom.Coordinates;
+
+                        lastX = -1;
+                        lastY = -1;
+
+                        foreach (var z in k) {
+                            float lon = (float)z.X;
+                            float lat = (float)z.Y;
+                            if (lat < gc.MaxLatitude && lat > gc.MinLatitude && lon < gc.MaxLongitude && lon > gc.MinLongitude) {
+                                var xy = gc.latlon2xy(lat, lon);
+                                float cx = (float)xy.Item1;
+                                float cy = (float)xy.Item2;
+                                if (
+                                    ( lastX != -1 && lastY != -1 ) && 
+                                    ( cx > 0 && cy > 0 ) &&
+                                    ( cx < bmp.Width && cy < bmp.Height ) &&
+                                    ( lastX > 0 && lastY > 0) &&
+                                    ( lastX < bmp.Width && lastY < bmp.Height)
+                                ){
+                                    graphics.DrawLine(pen, lastX, lastY, cx, cy);
+                                }
+                                lastX = cx;
+                                lastY = cy;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
