d62c8e5b5bdff68a7075bda1876dee214592f0d8
Lucas Teske <lucas@teske.net.br>
2017-05-05 22:18:16 -0300

    Handle exceptions that may happen on Directory.GetFiles

--- XRIT/Tools/Organizer.cs
+++ XRIT/Tools/Organizer.cs
@@ -29,167 +29,171 @@ namespace OpenSatelliteProject {
         }
 
         public void Update() {
-            List<string> files = Directory.GetFiles(folder).Where(f => f.EndsWith(".lrit")).ToList();
-            foreach (string file in files) {
-                if (alreadyProcessed.Contains(file)) {
-                    continue;
-                }
-                try {
-                    var header = FileParser.GetHeaderFromFile(file);
-                    var anciliary = header.AncillaryHeader != null ? header.AncillaryHeader.Values : null;
-                    var satellite = "Unknown";
-                    var region = "Unknown";
-                    var satLon = 0f;
-                    var datetime = header.TimestampHeader.DateTime; // Defaults to capture time
-                    var channel = 99;
-                    var segmentId = header.SegmentIdentificationHeader != null ? header.SegmentIdentificationHeader.Sequence : 0;
-
-                    if (header.Product.ID == (int)NOAAProductID.HIMAWARI8_ABI) {
-                        channel = header.SubProduct.ID;
-                        satellite = "HIMAWARI8";
-                        region = "Full Disk";
+            try {
+                List<string> files = Directory.GetFiles(folder).Where(f => f.EndsWith(".lrit")).ToList();
+                foreach (string file in files) {
+                    if (alreadyProcessed.Contains(file)) {
+                        continue;
                     }
+                    try {
+                        var header = FileParser.GetHeaderFromFile(file);
+                        var anciliary = header.AncillaryHeader != null ? header.AncillaryHeader.Values : null;
+                        var satellite = "Unknown";
+                        var region = "Unknown";
+                        var satLon = 0f;
+                        var datetime = header.TimestampHeader.DateTime; // Defaults to capture time
+                        var channel = 99;
+                        var segmentId = header.SegmentIdentificationHeader != null ? header.SegmentIdentificationHeader.Sequence : 0;
 
-                    var rgx = new Regex(@".*\((.*)\)", RegexOptions.IgnoreCase);
-                    var regMatch = rgx.Match(header.ImageNavigationHeader.ProjectionName);
-                    satLon = float.Parse(regMatch.Groups[1].Captures[0].Value, CultureInfo.InvariantCulture);
-
-                    if (anciliary != null) {
-                        if (anciliary.ContainsKey("Satellite")) {
-                            satellite = anciliary["Satellite"];
+                        if (header.Product.ID == (int)NOAAProductID.HIMAWARI8_ABI) {
+                            channel = header.SubProduct.ID;
+                            satellite = "HIMAWARI8";
+                            region = "Full Disk";
                         }
 
-                        if (anciliary.ContainsKey("Region")) {
-                            region = anciliary["Region"];
-                        }
+                        var rgx = new Regex(@".*\((.*)\)", RegexOptions.IgnoreCase);
+                        var regMatch = rgx.Match(header.ImageNavigationHeader.ProjectionName);
+                        satLon = float.Parse(regMatch.Groups[1].Captures[0].Value, CultureInfo.InvariantCulture);
 
-                        if (anciliary.ContainsKey("Channel")) {
-                            channel = int.Parse(anciliary["Channel"]);
-                        }
-
-                        if (anciliary.ContainsKey("Time of frame start")) {
-                            var dtstring = anciliary["Time of frame start"];
-                            // 2017/055/05:45:18
-                            // or
-                            // 2017-03-27T15:45:38.2Z
-                            if (dtstring[4] == '/') {
-                                var year = dtstring.Substring(0, 4);
-                                var dayOfYear = dtstring.Substring(5, 3);
-                                var hours = dtstring.Substring(9, 2);
-                                var minutes = dtstring.Substring(12, 2);
-                                var seconds = dtstring.Substring(15, 2);
-                                //Console.WriteLine("Year: {0}\nDay Of Year: {1}\nHours: {2}\nMinutes: {3}\nSeconds: {4}", year, dayOfYear, hours, minutes, seconds);
-                                datetime = new DateTime(int.Parse(year), 1, 1, int.Parse(hours), int.Parse(minutes), int.Parse(seconds));
-                                datetime = datetime.AddDays(int.Parse(dayOfYear));
-                            } else {
-                                datetime = DateTime.Parse(dtstring, null, DateTimeStyles.RoundtripKind);
+                        if (anciliary != null) {
+                            if (anciliary.ContainsKey("Satellite")) {
+                                satellite = anciliary["Satellite"];
                             }
-                        } else {
-                            Console.WriteLine("No Frame Time of Start found! Using capture time.");
-                        }
-                    }
 
-                    var cropSection = region.ToLower().Contains("full disk") || header.IsFullDisk;
-                    var timestamp = (int)Math.Floor((datetime - UnixEpoch).TotalSeconds);
+                            if (anciliary.ContainsKey("Region")) {
+                                region = anciliary["Region"];
+                            }
 
-                    if (timestamp < 0 && file.Contains("IMG_DK")) {
-                        // Himawari-8 relay BUG
-                        //IMG_DK01VIS_201704161550
-                        string bfile = Path.GetFileName(file);
-                        string hdt = bfile.Substring(12, 12);
-                        var year = hdt.Substring(0, 4);
-                        var month = hdt.Substring(4, 2);
-                        var day = hdt.Substring(6, 2);
-                        var hour = hdt.Substring(8, 2);
-                        var minute = hdt.Substring(10, 2);
-                        datetime = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day), int.Parse(hour), int.Parse(minute), 0);
-                        timestamp = (int)Math.Floor((datetime - UnixEpoch).TotalSeconds);
-                    }
+                            if (anciliary.ContainsKey("Channel")) {
+                                channel = int.Parse(anciliary["Channel"]);
+                            }
 
-                    if (!groupData.ContainsKey(timestamp)) {
-                        groupData[timestamp] = new GroupData();
-                    }
-                    var grp = groupData[timestamp];
-                    grp.SatelliteName = satellite;
-                    grp.RegionName = region;
-                    grp.FrameTime = datetime;
-                    grp.CropImage = cropSection;
-                    grp.SatelliteLongitude = satLon;
-                    grp.ColumnScalingFactor = header.ImageNavigationHeader.ColumnScalingFactor;
-                    grp.LineScalingFactor = header.ImageNavigationHeader.LineScalingFactor;
-                    grp.ColumnOffset = grp.ColumnOffset == -1 ? header.ImageNavigationHeader.ColumnOffset : grp.ColumnOffset;
-                    grp.LineOffset = grp.LineOffset == -1 ? header.ImageNavigationHeader.LineOffset : grp.LineOffset;
-
-                    var od = new OrganizerData();
-                    switch (channel) {
-                        case 1: // Visible
-                            od = grp.Visible;
-                            break;
-                        case 2: // Visible for G16
-                            if (satellite == "G16") {
-                                od = grp.Visible;
-                            } else {
-                                string p = $"{timestamp%1000}-{((NOAAProductID)header.Product.ID).ToString()}-{header.SubProduct.Name}";
-                                if (!grp.OtherData.ContainsKey(p)) {
-                                    grp.OtherData.Add(p, new OrganizerData());
+                            if (anciliary.ContainsKey("Time of frame start")) {
+                                var dtstring = anciliary["Time of frame start"];
+                                // 2017/055/05:45:18
+                                // or
+                                // 2017-03-27T15:45:38.2Z
+                                if (dtstring[4] == '/') {
+                                    var year = dtstring.Substring(0, 4);
+                                    var dayOfYear = dtstring.Substring(5, 3);
+                                    var hours = dtstring.Substring(9, 2);
+                                    var minutes = dtstring.Substring(12, 2);
+                                    var seconds = dtstring.Substring(15, 2);
+                                    //Console.WriteLine("Year: {0}\nDay Of Year: {1}\nHours: {2}\nMinutes: {3}\nSeconds: {4}", year, dayOfYear, hours, minutes, seconds);
+                                    datetime = new DateTime(int.Parse(year), 1, 1, int.Parse(hours), int.Parse(minutes), int.Parse(seconds));
+                                    datetime = datetime.AddDays(int.Parse(dayOfYear));
+                                } else {
+                                    datetime = DateTime.Parse(dtstring, null, DateTimeStyles.RoundtripKind);
                                 }
-                                od = grp.OtherData[p];
-                            }
-                            break;
-                        case 3: // Water Vapour
-                            if (satellite == "HIMAWARI8") {
-                                od = grp.Infrared;
                             } else {
-                                od = grp.WaterVapour;
+                                Console.WriteLine("No Frame Time of Start found! Using capture time.");
                             }
-                            break;
-                        case 4: // Infrared
-                            od = grp.Infrared;
-                            break;
-                        case 7: 
-                            if (satellite == "HIMAWARI8") {
-                                od = grp.WaterVapour;
-                            }
-                            break;
-                        case 8:
-                            if (satellite == "G16") {
-                                od = grp.WaterVapour;
-                            }
-                            break;
-                        case 13: // Infrared for G16
-                            if (satellite == "G16") {
-                                od = grp.Infrared;
-                            }
-                            break;
-                        default:
-                            string z = $"{timestamp%1000}-{((NOAAProductID)header.Product.ID).ToString()}-{header.SubProduct.Name}";
-                            if (!grp.OtherData.ContainsKey(z)) {
-                                grp.OtherData.Add(z, new OrganizerData());
-                            }
-                            od = grp.OtherData[z];
-                            break;
-                    } 
-
-
-                    od.Segments[segmentId] = file;
-                    if (od.Columns == -1) {
-                        od.Columns = header.ImageStructureHeader.Columns;
-                        od.Lines = header.ImageStructureHeader.Lines;
-                        od.PixelAspect = header.ImageNavigationHeader.ColumnScalingFactor / (float)header.ImageNavigationHeader.LineScalingFactor;
-                        od.ColumnOffset = header.ImageNavigationHeader.ColumnOffset;
-                        if (header.SegmentIdentificationHeader != null) {
-                            od.MaxSegments = header.SegmentIdentificationHeader.MaxSegments;
-                        } else {
-                            od.MaxSegments = 1;
                         }
-                    } else {
-                        od.Lines += header.ImageStructureHeader.Lines;
+
+                        var cropSection = region.ToLower().Contains("full disk") || header.IsFullDisk;
+                        var timestamp = (int)Math.Floor((datetime - UnixEpoch).TotalSeconds);
+
+                        if (timestamp < 0 && file.Contains("IMG_DK")) {
+                            // Himawari-8 relay BUG
+                            //IMG_DK01VIS_201704161550
+                            string bfile = Path.GetFileName(file);
+                            string hdt = bfile.Substring(12, 12);
+                            var year = hdt.Substring(0, 4);
+                            var month = hdt.Substring(4, 2);
+                            var day = hdt.Substring(6, 2);
+                            var hour = hdt.Substring(8, 2);
+                            var minute = hdt.Substring(10, 2);
+                            datetime = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day), int.Parse(hour), int.Parse(minute), 0);
+                            timestamp = (int)Math.Floor((datetime - UnixEpoch).TotalSeconds);
+                        }
+
+                        if (!groupData.ContainsKey(timestamp)) {
+                            groupData[timestamp] = new GroupData();
+                        }
+                        var grp = groupData[timestamp];
+                        grp.SatelliteName = satellite;
+                        grp.RegionName = region;
+                        grp.FrameTime = datetime;
+                        grp.CropImage = cropSection;
+                        grp.SatelliteLongitude = satLon;
+                        grp.ColumnScalingFactor = header.ImageNavigationHeader.ColumnScalingFactor;
+                        grp.LineScalingFactor = header.ImageNavigationHeader.LineScalingFactor;
+                        grp.ColumnOffset = grp.ColumnOffset == -1 ? header.ImageNavigationHeader.ColumnOffset : grp.ColumnOffset;
+                        grp.LineOffset = grp.LineOffset == -1 ? header.ImageNavigationHeader.LineOffset : grp.LineOffset;
+
+                        var od = new OrganizerData();
+                        switch (channel) {
+                            case 1: // Visible
+                                od = grp.Visible;
+                                break;
+                            case 2: // Visible for G16
+                                if (satellite == "G16") {
+                                    od = grp.Visible;
+                                } else {
+                                    string p = $"{timestamp%1000}-{((NOAAProductID)header.Product.ID).ToString()}-{header.SubProduct.Name}";
+                                    if (!grp.OtherData.ContainsKey(p)) {
+                                        grp.OtherData.Add(p, new OrganizerData());
+                                    }
+                                    od = grp.OtherData[p];
+                                }
+                                break;
+                            case 3: // Water Vapour
+                                if (satellite == "HIMAWARI8") {
+                                    od = grp.Infrared;
+                                } else {
+                                    od = grp.WaterVapour;
+                                }
+                                break;
+                            case 4: // Infrared
+                                od = grp.Infrared;
+                                break;
+                            case 7: 
+                                if (satellite == "HIMAWARI8") {
+                                    od = grp.WaterVapour;
+                                }
+                                break;
+                            case 8:
+                                if (satellite == "G16") {
+                                    od = grp.WaterVapour;
+                                }
+                                break;
+                            case 13: // Infrared for G16
+                                if (satellite == "G16") {
+                                    od = grp.Infrared;
+                                }
+                                break;
+                            default:
+                                string z = $"{timestamp%1000}-{((NOAAProductID)header.Product.ID).ToString()}-{header.SubProduct.Name}";
+                                if (!grp.OtherData.ContainsKey(z)) {
+                                    grp.OtherData.Add(z, new OrganizerData());
+                                }
+                                od = grp.OtherData[z];
+                                break;
+                        } 
+
+
+                        od.Segments[segmentId] = file;
+                        if (od.Columns == -1) {
+                            od.Columns = header.ImageStructureHeader.Columns;
+                            od.Lines = header.ImageStructureHeader.Lines;
+                            od.PixelAspect = header.ImageNavigationHeader.ColumnScalingFactor / (float)header.ImageNavigationHeader.LineScalingFactor;
+                            od.ColumnOffset = header.ImageNavigationHeader.ColumnOffset;
+                            if (header.SegmentIdentificationHeader != null) {
+                                od.MaxSegments = header.SegmentIdentificationHeader.MaxSegments;
+                            } else {
+                                od.MaxSegments = 1;
+                            }
+                        } else {
+                            od.Lines += header.ImageStructureHeader.Lines;
+                        }
+                        alreadyProcessed.Add(file);
+                    } catch (Exception e) {
+                        Console.WriteLine("Error reading file {0}: {1}", file, e);
+                        alreadyProcessed.Add(file);
                     }
-                    alreadyProcessed.Add(file);
-                } catch (Exception e) {
-                    Console.WriteLine("Error reading file {0}: {1}", file, e);
-                    alreadyProcessed.Add(file);
                 }
+            } catch (Exception e) {
+                Console.WriteLine("Error checking folders {0}", e);
             }
         }
     }
