ab5b2eaaf70e4ed9e2cc68d7da22bf05e8e64292
Lucas Teske <lucas@teske.net.br>
2017-09-06 02:01:28 -0300

    Fixed DateTime being local instead UTC
    *	Closes #64

--- XRIT/Tools/LLTools.cs
+++ XRIT/Tools/LLTools.cs
@@ -62,8 +62,8 @@ namespace OpenSatelliteProject.Tools {
 
         public static DateTime UnixTimeStampToDateTime(long unixTimeStamp) {
             // Unix timestamp is seconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
             return dtDateTime;
         }
 
